a7331fdecda2d5f0b26e8f504f3933265ea8bb69
andrii910320 <128429619+andrii910320@users.noreply.github.com>
2025-03-04 13:07:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    // credentials: true,
   })
 );
 app.use(express.json());
